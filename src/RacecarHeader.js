58542f8ae5a88b58644d9a7401bd5d98e5c83a8a
tomatillodesign <chris@tomatillodesign.com>
2019-12-16 09:50:28 -0500

    fixed animation

--- src/RacecarHeader.js
+++ src/RacecarHeader.js
@@ -7,19 +7,54 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function RacecarHeader() {
-  return (
-    <header className="App-header">
-    <div className="header-icons">
-     <FontAwesomeIcon icon={faCarSide} />
-    </div>
-      <a href="http://racecarmath.org/" onClick={e =>
-       window.confirm(
-          "Are you sure you want to start over? Your name and score will be erased from this page."
-       )
-      }><h1>Racecar Math</h1></a>
-    </header>
-  );
+class RacecarHeader extends React.Component {
+
+     constructor(props) {
+         super(props);
+         this.state = {
+              timer: 0,
+              showIcon: true,
+         };
+  }
+
+componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      100
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+       this.setState(prevState => {
+        return { timer: prevState.timer + 1 };
+      });
+  }
+
+  render() {
+
+
+       return (
+         <header className="App-header">
+
+         { this.state.timer <= 62 &&
+             <div className="header-icons">
+              <FontAwesomeIcon icon={faCarSide} />
+             </div>
+          }
+
+           <a href="http://racecarmath.org/" onClick={e =>
+            window.confirm(
+               "Are you sure you want to start over? Your name and score will be erased from this page."
+            )
+           }><h1>Racecar Math</h1></a>
+         </header>
+       );
+     }
+
 }
 
 export default RacecarHeader;
